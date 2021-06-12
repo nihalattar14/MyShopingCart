9a2341faddc7e9545d9dd2316a6c33fd2746a32a
nihalattar14 <nihalattar14@gmail.com>
2021-06-13 00:19:00 +0530

    Products featching

--- MyShopingCart/shop/views.py
+++ MyShopingCart/shop/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+from math import ceil
 # Create your views here.
 
+
+
+
 def index(request):
-    return render(request,'shop/index.html')
+
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    # params = {'no_of_slide':nSlides,'range':range(1,nSlides),'product':products}
+    allprods = [[products,range(1,nSlides),nSlides],\
+               [products,range(1,nSlides),nSlides]]
+    print(allprods)
+    params = {'allProds':allprods}
+    return render(request,'shop/index.html',params)
 
 
 def about(request):
-    return HttpResponse("We are at about")
-
+    return render(request,'shop/about.html')
 def contact(request):
     return HttpResponse("We are at contact")
 
