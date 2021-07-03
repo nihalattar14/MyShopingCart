1a0baa59478424595cda608b1e275e0641df8885
nihalattar14 <nihalattar14@gmail.com>
2021-07-03 22:46:58 +0530

    Much new like all pages

--- MyShopingCart/shop/views.py
+++ MyShopingCart/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 # Create your views here.
 
@@ -9,31 +9,56 @@ from math import ceil
 
 def index(request):
 
-    products = Product.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n//4 + ceil((n/4)-(n//4))
+    # print("request>>>>",request)
+
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
     # params = {'no_of_slide':nSlides,'range':range(1,nSlides),'product':products}
-    allprods = [[products,range(1,nSlides),nSlides],\
-               [products,range(1,nSlides),nSlides]]
-    print(allprods)
+    # allprods = [[products,range(1,nSlides),nSlides],[products,range(1,nSlides),nSlides]]
+
+    allprods = []    
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allprods.append([prod, range(1, nSlides), nSlides])
+
+    # print(allprods)
     params = {'allProds':allprods}
     return render(request,'shop/index.html',params)
 
 
 def about(request):
     return render(request,'shop/about.html')
+
+
 def contact(request):
-    return HttpResponse("We are at contact")
+    if request.method == "POST":
+
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+
+    return render(request,'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request,'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request,'shop/search.html')
 
-def productView(request):
-    return HttpResponse("We are at productview")
+def productView(request,prodid):
+    # Fetch the product using product ID
+    product = Product.objects.filter(id = prodid)
+    print("product>>>>>>>>>>>>>>",product)
+    return render(request,'shop/productview.html',{'product': product[0]})
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request,'shop/checkout.html')
